46e7014accd67fe30b5c52c3d006a8048e73fcae
stEl1as <kuz.ilyazool@gmail.com>
2021-09-21 10:47:43 +0300

    second

--- testapp.js
+++ testapp.js
@@ -1,27 +1,5 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = requaire ('express')
+const app = express()
+const PORT = process.env.PORT || 80
+app.get ('/' (req,res)=>{res.end ('<h1>Home</h1>')})
+app.listen(PORT, ()=>{console.log('server has been started...')})
